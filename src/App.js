fbdea36da89344a63751a61add6afe19789f3197
hansbastiaan <hans.bastiaan@gmail.com>
2022-02-18 09:04:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App() {
-  const [name, setName] = useState("");
-  const [headingText, setHeadingText] = useState("");
-
-  const handleChange = (event) => {
-    setName(event.target.value);
-  };
-
-  const handleClick = (event) => {
-    event.preventDefault();
-    setHeadingText(name);
-  };
-
   return (
     <div className="container">
-      <h1>Hello {headingText}!</h1>
-      <form onSubmit={handleClick}>
-        <input
-          onChange={handleChange}
-          type="text"
-          placeholder="What's your name?"
-          value={name}
-        />
-        <button type="submit">Submit</button>
+      <h1>Hello</h1>
+      <form>
+        <input name="fName" placeholder="First Name" />
+        <input name="lName" placeholder="Last Name" />
+        <button>Submit</button>
       </form>
     </div>
   );
