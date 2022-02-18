ffba047226620dfab1fce7b615a1aff4aafbb1db
hansbastiaan <hans.bastiaan@gmail.com>
2022-02-18 10:57:56 +0100

    object to handle

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import React, { useState } from "react";
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState({
+    firstName: "",
+    lastName: ""
+  });
 
-  const updateFirstName = (event) => {
-    const inputFirstName = event.target.value;
-    setFirstName(inputFirstName);
-  };
+  const handleChange = (event) => {
+    const { value, name } = event.target;
 
-  const updateLastName = (event) => {
-    const inputLastName = event.target.value;
-    setLastName(inputLastName);
+    setFullName((prevValue) => {
+      if (name === "firstName") {
+        return {
+          firstName: value,
+          lastName: prevValue.lastName
+        };
+      } else if (name === "lastName") {
+        return {
+          firstName: prevValue.firstName,
+          lastName: value
+        };
+      }
+    });
   };
 
   return (
     <div className="container">
       <h1>
-        Hello {firstName} {lastName}
+        Hello {fullName.firstName} {fullName.lastName}
       </h1>
-      <form onSubmit>
+      <form>
         <input
-          onChange={updateFirstName}
+          onChange={handleChange}
           name="firstName"
           placeholder="First Name"
-          value={firstName}
+          value={fullName.firstName}
         />
         <input
-          onChange={updateLastName}
+          onChange={handleChange}
           name="lastName"
           placeholder="Last Name"
-          value={lastName}
+          value={fullName.lastName}
         />
         <button>Submit</button>
       </form>
