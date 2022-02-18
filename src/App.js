e44463f0fb174a0722825b320b8a69b3e05e6e9a
hansbastiaan <hans.bastiaan@gmail.com>
2022-02-18 10:20:29 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const updateFirstName = (event) => {
+    const inputFirstName = event.target.value;
+    setFirstName(inputFirstName);
+  };
+
+  const updateLastName = (event) => {
+    const inputLastName = event.target.value;
+    setLastName(inputLastName);
+  };
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+      <h1>
+        Hello {firstName} {lastName}
+      </h1>
+      <form onSubmit>
+        <input
+          onChange={updateFirstName}
+          name="firstName"
+          placeholder="First Name"
+          value={firstName}
+        />
+        <input
+          onChange={updateLastName}
+          name="lastName"
+          placeholder="Last Name"
+          value={lastName}
+        />
         <button>Submit</button>
       </form>
     </div>
